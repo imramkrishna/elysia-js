a4ef9dcf7eeaf57d1e3443e612f3f7617cb4b190
Ram Krishna Yadav <itsramky234@gmail.com>
2025-11-17 22:35:15 +0545

    added norraml http server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+app.post("/login",({body})=>{
+  console.log(body);
+})
 
+app.get("/", () => "Hello Elysia")
+app.get("/greet",()=>"Good Morning..")
+app.listen(3000)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
